ecb0e8eafb0c45bab041f0932b75e24f9e4ec6bd
katielewis <katie@codeforamerica.org>
2013-11-05 11:52:31 -0800

    adjusted overlay so it would be visible on mobile

--- js/trailhead.js
+++ js/trailhead.js
@@ -234,18 +234,18 @@ function startup() {
   // Kick things off
 
   var overlayHTMLIE = "<h1>Welcome to To The Trails!</h1>" + 
-      "<p>We're sorry, but To The Trails is not compatible with Microsoft Internet Explorer 8 or earlier." + 
+      "<p>We're sorry, but To The Trails is not compatible with Microsoft Internet Explorer 8 or earlier versions of that web browser." + 
       "<p>Please upgrade to the latest version of " +
       "<a href='http://windows.microsoft.com/en-us/internet-explorer/download-ie'>Internet Explorer</a>, " + 
       "<a href='http://google.com/chrome'>Google Chrome</a>, or " +  
       "<a href='http://getfirefox.com'>Mozilla Firefox</a>." +
-      "<p>If you are currently running Windows XP, you'll need to upgrade to Chrome or Firefox." +
+      "<p>If you are currently using Windows XP, you'll need to download and use Chrome or Firefox." +
       "<img src='/img/Overlay-Image-01.png' alt='trees'>";
 
   var overlayHTML = "<span class='closeOverlay'>x</span>" +
     "<h1>Welcome To The Trails!</h1>" +
     "<p>To The Trails is currently in public beta, so it still a work in progress. We'd love to hear how this site is working for you, so we can make it even better." +
-    "<p>Send your feedback to " +
+    "<p>Send feedback and report bugs to " +
     "<a href='mailto:hello@tothetrails.com?Subject=Feedback' target='_top'>hello@tothetrails.com</a>.";
 
   var closedOverlayHTML = "<h1>Come visit us Nov 13th!</h1>" +
